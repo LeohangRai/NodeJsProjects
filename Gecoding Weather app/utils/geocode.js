7fa105175e6fd86b757cf04846a557e29985a177
Leohang Rai <raileohang@gmail.com>
2021-08-13 09:36:47 +0545

    Updated the utility folder. Worked on the geocode and weather modules.

--- Gecoding Weather app/utils/geocode.js
+++ Gecoding Weather app/utils/geocode.js
@@ -1,22 +1,30 @@
 const request = require('request')
 
+//NOTE
+//Callback function format is callback(error, response)
+//When error occurs, response must be NULL and vice versa.
 
 function geocode(address, callback){
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoibGVvaGFuZ3JhaSIsImEiOiJja3M1djczZTUwYmNuMnVvZGdvbHlzb2xsIn0.bIwV61voTm0ev1dqgDmCwA`
+    
+    //json:true makes the req function return the reponse in json format
     request({url:url, json:true}, (error, response) =>{
+        //Passing undefined as the second arg during errors because the value of response will be NULL when error occurs
+        //error handling for physical/system errors
         if (error){
-            //passing undefined as second arg for callback because when error occurs the response will be undefined
             callback("Oops! Unable to connect to the Geocode service.\nCheck your Internet connection maybe?", undefined)
         }
+        //error handling for bad response, when the user Input is wrong/bad
         else if (response.body.features.length === 0){
-            //passing undefined as second arg because of the same reason as above
             callback("Oops! Did you type the name of your location properly? Try again please.", undefined)
         }
+        //No error
         else{
-            //passing undefined as first arg of the callback because it the error will be NULL when the response is obtained
+            //Grabbing the latitude, longitude and location values from the JSON response coming through the API
             const latitude = response.body.features[0].center[0]
             const longitude = response.body.features[0].center[1]
             const location = response.body.features[0].place_name
+            //Passing undefined as the first arg, because the value of 'error' must be NULL in this case when the user calls the callback function
             callback(undefined, {'latitude': latitude, 'longitude':longitude, 'location': location})
         }
     })
